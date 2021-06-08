87053a1e450b89b54b5d6d62fb98f0b42cbddc22
polaris-labs <yamate.konoha@gmail.com>
2021-06-08 13:25:16 -0400

    fix import error & config

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -20,7 +20,7 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Metadata.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/introspection/ERC165.sol";
-import "@openzeppelin/contracts/math/utils/SafeMath.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/EnumerableMap.sol";
